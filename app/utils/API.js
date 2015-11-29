85b22f6c80c6c6da31e3252761cbc55e7c653083
renatn <renatn@gmail.com>
2015-11-29 22:57:29 +0300

    API: current, geek

--- app/utils/API.js
+++ app/utils/API.js
@@ -1,5 +1,6 @@
 import request from 'superagent';
 
+const PREFIX_API = 'http://master.radio-t.com:8778/api/v1';
 
 const hostname = (url) => {
     const parser = document.createElement('a');
@@ -7,13 +8,11 @@ const hostname = (url) => {
     return parser.hostname;
 };
 
-const pad0 = (value) => {
-	return value < 10 ? '0' + value : value;
-}
+const pad0 = value => value < 10 ? '0' + value : value;
 
 const datetime = (ts) => {
 	const dt = new Date(ts);
-	return dt.getFullYear() + '-' + pad0(dt.getMonth()) + '-' + pad0(dt.getDate()) + ' ' + pad0(dt.getHours()) + ':'+pad0(dt.getMinutes());
+	return dt.getFullYear() + '-' + pad0(dt.getMonth()+1) + '-' + pad0(dt.getDate()) + ' ' + pad0(dt.getHours()) + ':'+pad0(dt.getMinutes());
 };
 
 export default class API {
@@ -21,7 +20,7 @@ export default class API {
 	static news(username, password) {
   		return new Promise((resolve, reject) => {
 	        request
-	            .get('http://master.radio-t.com:8778/api/v1/news')
+	            .get(PREFIX_API+'/news')
 	            .end((err, res) => {
 	                const news = res.body.map(n => {
 	                    return {
@@ -32,8 +31,47 @@ export default class API {
 	                });
 	                resolve(news);
 	            });
-    	})
+    	});
     }
 
+    static makeNewsActive(id) {
+  		return new Promise((resolve, reject) => {
+	        request
+	            .put(PREFIX_API+'/news/active/'+id)
+	            .end((err, res) => {
+	                resolve(res.body);
+	            });
+    	});
+    }
+
+    static toggleNewsGeekFlag(id, isGeek) {
+    	const endpoint = isGeek ? 'nogeek' : 'geek';
+  		return new Promise((resolve, reject) => {
+	        request
+	            .put(PREFIX_API+'/news/'+endpoint+'/'+id)
+	            .end((err, res) => {
+	                resolve(res.body);
+	            });
+    	});
+    }
+
+    static toggleNewsDeletedFlag(id, isDeleted) {
+  		return new Promise((resolve, reject) => {
+			if (isDeleted) {
+		        request
+		            .put(PREFIX_API+'/news/undelete/'+id)
+		            .end((err, res) => {
+		                resolve(res.body);
+		            });
+	 		} else {
+		        request
+		            .del(PREFIX_API+'/news/'+id)
+		            .end((err, res) => {
+		                resolve(res.body);
+		            });
+			} 
+    	});    	
+	 
+    }
 
 }
